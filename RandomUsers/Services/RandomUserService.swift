7fbf58e64a78c7ec3deca0967be039d3b2f06d6c
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-07-01 09:13:00 -0600

    Consuming random user api
    Initial implementation of service with some error handling.
    Fetch users depending on number of user that want to be displayed

---
+++ RandomUsers/Services/RandomUserService.swift
@@ -0,0 +1,45 @@
+//
+//  RandomUserService.swift
+//  RandomUsers
+//
+//  Created by Jorge Ramos on 01/07/25.
+//
+import Foundation
+struct RandomUserService {
+    private let baseURL = "http://randomuser.me/"
+    func fetchRandomUsersGrouped(numberOfUsers: Int) async throws -> [User] {
+        let urlString = "\(baseURL)?results=\(numberOfUsers)"
+        guard let url = URL(string: urlString) else {
+            throw UserAPIError.invalidURL
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+            throw UserAPIError.requestFailed
+        }
+        do {
+            let userResponse = try JSONDecoder().decode(UserResponse.self, from: data)
+            return userResponse.results
+        }
+        catch {
+            throw UserAPIError.decodingFailed
+        }
+    }
+        
+    
+}
+
+enum UserAPIError: Error, LocalizedError, Equatable {
+    case invalidURL
+    case requestFailed
+    case decodingFailed
+    case serverError(String)
+
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL: return "Invalid User API URL."
+        case .requestFailed: return "Request to User API failed."
+        case .decodingFailed: return "Failed to decode User API response."
+        case .serverError(let message): return "User API error: \(message)"
+        }
+    }
+}
