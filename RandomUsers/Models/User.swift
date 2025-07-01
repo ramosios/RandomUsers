5acf5f6537edc579a3725e2e953f97ddfcd8994a
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-07-01 08:19:42 -0600

    Add User Model
    created user model based on api json response

---
+++ RandomUsers/Models/User.swift
@@ -0,0 +1,57 @@
+//
+//  User.swift
+//  RandomUsers
+//
+//  Created by Jorge Ramos on 01/07/25.
+//
+struct UserResponse: Decodable {
+    let results: [User]
+}
+
+struct User: Decodable {
+    let gender: String
+    let name: Name
+    let location: Location
+    let email: String
+    let login: Login
+    let dob: String
+    let registered: String
+    let phone: String
+    let cell: String
+    let id: ID
+    let picture: Picture
+    let nat: String
+}
+
+struct Name: Decodable {
+    let title: String
+    let first: String
+    let last: String
+}
+
+struct Location: Decodable {
+    let street: String
+    let city: String
+    let state: String
+    let postcode: String
+}
+
+struct Login: Decodable {
+    let username: String
+    let password: String
+    let salt: String
+    let md5: String
+    let sha1: String
+    let sha256: String
+}
+
+struct ID: Decodable {
+    let name: String
+    let value: String?
+}
+
+struct Picture: Decodable {
+    let large: String
+    let medium: String
+    let thumbnail: String
+}
