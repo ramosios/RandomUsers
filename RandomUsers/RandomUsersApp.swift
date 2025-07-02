02f222dc7c86a99eb67f55dfc84dd1a598b9349e
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-07-02 11:18:30 -0600

    Infinite scroll functionality
    - When reaching last user loads 10 more

--- RandomUsers/RandomUsersApp.swift
+++ RandomUsers/RandomUsersApp.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 @main
-struct RandomUsersApp: App {
+struct RandomUsersApp: SwiftUI.App {
     @StateObject private var viewModel = UserListViewModel()
+    private let persistenceManager = try? UserPersistenceManager()
+
     var body: some Scene {
         WindowGroup {
             UserListView()
                 .environmentObject(viewModel)
+                .task {
+                    if let savedUsers = persistenceManager?.load(), savedUsers.isEmpty {
+                        await viewModel.fetchUsers(count: 20)
+                    } else if let savedUsers = persistenceManager?.load() {
+                        viewModel.users = savedUsers
+                    }
+                }
         }
     }
 }
